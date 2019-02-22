521a182158d4bb8d32c8045a02d3416a4abb6cf6
starryfei <760646630fei@gmail.com>
2019-02-23 00:24:37 +0800

    init element-ui

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    header
+    <el-row>
+    <el-col :span="24"><div class="grid-content bg-purple-dark"></div></el-col>
+    </el-row>
   </div>
 </template>
 <script>
@@ -14,6 +16,31 @@ export default {
   
 }
 </script>
-<style scope>
-
+<style>
+  .el-row {
+    margin-bottom: 0px;
+    }
+    last-child {
+      margin-bottom: 0;
+    }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import echarts from 'echarts'
 import App from './App'
 import router from './router'
-import echarts from 'echarts'
+
+Vue.use(ElementUI)
 Vue.prototype.$echarts = echarts
 
 Vue.config.productionTip = false
